bceaf26d51b8b1127601b2e541b6c6aef49bd308
jayati915 <79280856+jayati915@users.noreply.github.com>
2022-12-23 14:46:46 -0500

    Create Classy.py

---
+++ Classy.py
@@ -0,0 +1,64 @@
+"""You can use this class to represent how classy someone
+or something is.
+"Classy" is interchangable with "fancy".
+If you add fancy-looking items, you will increase
+your "classiness".
+Create a function in "Classy" that takes a string as
+input and adds it to the "items" list.
+Another method should calculate the "classiness"
+value based on the items.
+The following items have classiness points associated
+with them:
+"tophat" = 2
+"bowtie" = 4
+"monocle" = 5
+Everything else has 0 points.
+Use the test cases below to guide you!"""
+
+
+class Classy(object):
+    def __init__(self):
+        self.items = []
+        self.points = 0
+    
+    def addItem(self,string):
+        self.items.append(string)
+    
+    
+    def getClassiness(self):
+        try:
+            for i in self.items:
+                if i == "tophat":
+                    self.points+=2
+                if i == "bowtie":
+                    self.points+=4
+                if i == "monocle":
+                    self.points+=5
+                self.items.pop(0)
+            self.items.pop(0)
+        except IndexError as e:
+            pass
+        return self.points
+        
+        
+        
+
+# Test cases
+me = Classy()
+
+# Should be 0
+print (me.getClassiness())
+
+me.addItem("tophat")
+# Should be 2
+print (me.getClassiness())
+
+me.addItem("bowtie")
+me.addItem("jacket")
+me.addItem("monocle")
+# Should be 11
+print(me.getClassiness())
+
+me.addItem("bowtie")
+# Should be 15
+print (me.getClassiness())
